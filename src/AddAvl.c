0ef376e8d442674abe9ba1169a8c7e8b739016b3
Ho Guo Xian <hogouxian@gmail.com>
2017-10-29 20:48:33 +0800

    implemented insert node and rotating

--- src/AddAvl.c
+++ src/AddAvl.c
@@ -5,45 +5,29 @@ Node *addAvl(Node **rootPtr, Node *nodeToAdd){
     *rootPtr = nodeToAdd;
     return (*rootPtr);
   }
-  if(nodeToAdd->data > (*(rootPtr))->data){
-    // addAvl(&(*(rootPtr))->right, nodeToAdd);
-    if((*rootPtr)->right == NULL){
-      (*rootPtr)->right = nodeToAdd;
-      (*rootPtr)->bf++;
-      return (*rootPtr);
-    }
-    else{
-        (*rootPtr)->right = addAvl(&(*(rootPtr))->right, nodeToAdd);
-        (*rootPtr)->bf++;
-        if((*rootPtr)->bf >=2){
-          if((*rootPtr)->right->bf < 0){
-            return rotateRightLeft(*(rootPtr));
-          }
-          else{
-            return rotateLeft(*(rootPtr));
-          }
-        }
-
-    }
-  }
   else{
-    if((*rootPtr)->left == NULL){
-      (*rootPtr)->left = nodeToAdd;
-      (*rootPtr)->bf--;
-      return (*rootPtr);
+    if(nodeToAdd->data > (*(rootPtr))->data){
+      // addAvl(&(*(rootPtr))->right, nodeToAdd);
+      (*rootPtr)->right = addAvl(&(*(rootPtr))->right, nodeToAdd);
+      (*rootPtr)->bf++;
+      if(*(rootPtr)->bf > 1){
+        if((*rootPtr)->right->bf < 0){
+          //rotate right left
+        }
+        //rotate left
+      }
     }
     else{
-        addAvl(&(*(rootPtr))->left, nodeToAdd);
-        (*rootPtr)->bf --;
-        if((*rootPtr)->bf <=-2){
-          if((*rootPtr)->left->bf >0){
-            rotateleftRight(*(rootPtr));
-          }
-          else{
-            rotateRight(*(rootPtr));
-          }
+      (*rootPtr)->left = addAvl(&(*(rootPtr))->left, nodeToAdd);
+      (*rootPtr)->bf--;
+      if(*(rootPtr)->bf < -1){
+        if((*rootPtr)->left->bf > 0){
+          //rotate left right
         }
+        //rotate right
       }
+    }
   }
+
 
 }
